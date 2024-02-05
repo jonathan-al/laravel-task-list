51a1f40368e0b0b54cf66d82a92313287e2bf195
Jonathan Alcantara <jonathan.informatics@gmail.com>
2024-02-04 23:50:54 -0600

    set routes

--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,10 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
 
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {
-  }
-}
-
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
+use App\Jobs\DeleteTasksJob;
 
 /*
 |--------------------------------------------------------------------------
@@ -75,13 +25,16 @@ Route::get("/", function () {
     return redirect()->route("tasks.show");
 });
 
-Route::get("/tasks", function () use ($tasks) {
+Route::get("/tasks", function () {
     return view("tasks", [
-        "tasks"=> $tasks
+        // "tasks"=> \App\Models\Task::orderBy("id","asc")->get()
+        // "tasks"=> \App\Models\Task::all()
+        // "tasks"=> \App\Models\Task::latest("created_at")->where("completed", true)->get()
+        "tasks"=> Task::latest()->get()
     ]);
 })->name("tasks.show");
 
-Route::get("/tasks/{id}", function ($id) use ($tasks) {
+/* Route::get("/tasks/{id}", function ($id) use ($tasks) {
   $task = collect($tasks)->firstWhere("id", $id);
 
   if (!$task) {
@@ -90,7 +43,14 @@ Route::get("/tasks/{id}", function ($id) use ($tasks) {
   return view("task", [
       "task"=> $task
   ]);
-})->name("tasks.index");
+})->name("tasks.index"); */
+
+Route::get('tasks/{id}', function($id){
+  return view('task', [
+    // 'task' => \App\Models\Task::find($id)
+    'task' => Task::findOrFail($id)
+  ]);
+})->name('tasks.index');
 
 Route::get('/hello', function () {
     return "Hello";
@@ -107,3 +67,10 @@ Route::get("/greet/{name}", function ($name) {
 Route::fallback(function () {
     return redirect()->route("tasks.index");
 });
+
+Route::delete("/tasks/{task}", function(Task $task){
+  $task->delete();
+
+  return redirect()->route("tasks.index")
+    ->with("success", "Task deleted successfully!");
+})->name("task.destroy");
